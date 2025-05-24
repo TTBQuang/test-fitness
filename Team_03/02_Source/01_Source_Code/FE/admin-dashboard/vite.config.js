6f2ce35df9f463f328ae87193380adcf964cdc78
21120540 <buuquang102@gmail.com>
2025-05-24 21:42:47 +0700

    ..

--- Team_03/02_Source/01_Source_Code/FE/admin-dashboard/vite.config.js
+++ Team_03/02_Source/01_Source_Code/FE/admin-dashboard/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "frame-ancestors 'self' https://*.skycloak.io https://*.vercel.app;",
+    },
+  },
   // server: {
   //   proxy: {
   //     "/api": {

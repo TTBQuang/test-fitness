5682b591654dcb9d742a4e3ce39dfbc0b94e3cd4
21120540 <buuquang102@gmail.com>
2025-05-24 21:47:39 +0700

    .

--- Team_03/02_Source/01_Source_Code/FE/admin-dashboard/vite.config.js
+++ Team_03/02_Source/01_Source_Code/FE/admin-dashboard/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     headers: {
       "Content-Security-Policy":
-        "frame-ancestors 'self' https://*.skycloak.io https://*.vercel.app;",
+        "default-src 'self'; frame-ancestors 'self' https://*.skycloak.io https://*.vercel.app https://fcaaf0a4-fd3e-4274-8377-7a88e78f9086.app.skycloak.io; frame-src 'self' https://*.skycloak.io https://fcaaf0a4-fd3e-4274-8377-7a88e78f9086.app.skycloak.io; connect-src 'self' https://*.skycloak.io https://*.vercel.app;",
+      "X-Frame-Options":
+        "ALLOW-FROM https://fcaaf0a4-fd3e-4274-8377-7a88e78f9086.app.skycloak.io",
     },
   },
   // server: {
